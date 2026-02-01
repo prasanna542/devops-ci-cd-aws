7873bf3496dbdedf85243a7435026e3c16e17cb0
prasanna <prasanna542002@gmail.com>
2026-02-01 14:40:35 +0530

    Add Flask app and fix environment setup

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify(status="UP"), 200
+
+@app.route("/")
+def home():
+    return jsonify(message="DevOps CI/CD App", version="1.0.0")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
